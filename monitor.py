205d34c34fc0c38f263583fe6e8770fe49ff4636
John Barrett <johnjbarrett22@gmail.com>
2019-03-12 21:32:21 -0700

    Setting up email functionality, along with the environment variables to keep sensitive data secure

--- monitor.py
+++ monitor.py
@@ -1,4 +1,19 @@
+import os
+import smtplib
 import requests
+
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 r = requests.get('https://johnjbarrett.me', timeout = 5)
 
+if r.status_code != 200:
+    with smtplib.SMTP('smtp.gmail.com', 587) as smtp:
+        smtp.ehlo()
+        smtp.starttls()
+        smtp.ehlo()
+
+        smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+        
+        body = 'Make sure the server is restarted and is back up!'
+        subject = '~~~WEBSITE IS DOWN!~~~'
