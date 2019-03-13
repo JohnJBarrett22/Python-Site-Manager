0cd16a72e22090d895fca8f25344ec49e201a211
John Barrett <johnjbarrett22@gmail.com>
2019-03-13 16:37:20 -0700

    Added in linode connection logic

--- monitor.py
+++ monitor.py
@@ -1,10 +1,17 @@
 import os
 import smtplib
 import requests
+from linode_api4 import LinodeClient
 
 EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
 EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
+LINODE_TOKEN = os.environ.get('LINODE_TOKEN')
 RECEIVER = 'moo'
+
+client = LinodeClient(LINODE_TOKEN)
+
+for linode in client.linode.instances():
+    print(f'{linode.label}: {linode.id}')
 
 r = requests.get('https://johnjbarrett.me', timeout = 5)
 
